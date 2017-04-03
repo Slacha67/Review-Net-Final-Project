8c92263d71c378b5fc8a3a092b51b720d1a832bb
Sacha Ketchum <sacha.mancuso18@alumni.loyola.ca>
2017-04-03 13:39:16 -0400

    added comments to java
    comments in java

--- Java/Javalogin.js
+++ Java/Javalogin.js
@@ -1,27 +1,22 @@
+/* this is my function login that will be called in my html page*/
 function login(){
+	/*Here i am naming my variables for the login*/
 	var user = "Mancuso"
 	var pass = "Sacha"
 	
-	/*var prompt2 = prompt("Please enter your password");
-	console.log("haha")*/
-	
+	/* I am prompting the user enter their username and password*/
 	var userName=prompt("Username","")
 	var PassWord = prompt("Enter password","");
+	/* Here the if is checking to see if the username and passowrd is correct*/
 	if (userName === user) {
 		if (PassWord === pass){
+			/* if ikts true it will promt that the user is logged in and change a text saying the name of the user*/
 			document.getElementById("login").innerHTML = "Sacha Mancuso";
 			confirm ("welcome, you are logged in");
 		}else{
+			/* if the username or password is wrong the user will prompt saying its wrong*/
 			confirm("wrong username or password");
 			}
-		}else{
-			confirm("wrong username or password");
-			}
+	
 		
-	/**if (prompt1 === user){
-		if(prompt2 === pass){
-			confirm("You're logged in")
-		}
-		else confirm("please try again")
-	}*/
 }
