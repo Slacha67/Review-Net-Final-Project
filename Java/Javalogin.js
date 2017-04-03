609158a82e12caa789464389192bb5552be29336
Sacha Ketchum <sacha.mancuso18@alumni.loyola.ca>
2017-04-03 13:08:52 -0400

    added Java update
    Java update

--- Java/Javalogin.js
+++ Java/Javalogin.js
@@ -1,12 +1,27 @@
 function login(){
-	var user = "Mancusos"
-	var pass = "Cool"
-	var prompt1 = prompt("Please enter your username");
-	var prompt2 = prompt("Please enter your password");
-	console.log("haha")
-	if (prompt1 === user){
+	var user = "Mancuso"
+	var pass = "Sacha"
+	
+	/*var prompt2 = prompt("Please enter your password");
+	console.log("haha")*/
+	
+	var userName=prompt("Username","")
+	var PassWord = prompt("Enter password","");
+	if (userName === user) {
+		if (PassWord === pass){
+			document.getElementById("login").innerHTML = "Sacha Mancuso";
+			confirm ("welcome, you are logged in");
+		}else{
+			confirm("wrong username or password");
+			}
+		}else{
+			confirm("wrong username or password");
+			}
+		
+	/**if (prompt1 === user){
 		if(prompt2 === pass){
 			confirm("You're logged in")
 		}
-	}
+		else confirm("please try again")
+	}*/
 }
