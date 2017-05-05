7978432681cf969684e448aaa193d4ab3b0ba70f
Sacha Ketchum <sacha.mancuso18@alumni.loyola.ca>
2017-05-05 14:53:34 -0400

    added arrays

--- Java/Javalogin.js
+++ Java/Javalogin.js
@@ -1,22 +1,32 @@
-/* this is my function login that will be called in my html page*/
 function login(){
-	/*Here i am naming my variables for the login*/
-	var user = "Mancuso"
-	var pass = "Sacha"
 	
-	/* I am prompting the user enter their username and password*/
-	var userName=prompt("Username","")
-	var PassWord = prompt("Enter password","");
-	/* Here the if is checking to see if the username and passowrd is correct*/
-	if (userName === user) {
-		if (PassWord === pass){
-			/* if ikts true it will promt that the user is logged in and change a text saying the name of the user*/
-			document.getElementById("login").innerHTML = "Sacha Mancuso";
-			confirm ("welcome, you are logged in");
+	/** I am prompting the user enter their username and password**/
+	var user = prompt("Username")
+	var pass = prompt("Enter password","");
+	
+	
+	/**Here i am naming my variables for the login**/
+	
+	var userarray = ["Anthony", "Anthony2", "Anthony3", "Anthony4", "Sacha", "t"]
+	var passarray = ["Anania", "Smeros", "McCall", "Perna", "Mancuso", "r"]
+	
+	/**Here the if is checking to see if the username and passowrd is correct**/
+	
+	for (j = 0; j <= userarray.length; j++){
+		if ((user === userarray[j]) && (pass === passarray[j])){
+            log = true;
+            break;
+        }else{
+            log = false;
+        }
+    }
+			
+			
+		if (log === true){
+			document.getElementById("userarray").innerHTML = "Sup" + " " + userarray[j];
 		}else{
-			/* if the username or password is wrong the user will prompt saying its wrong*/
+			/**if the username or password is wrong the user will prompt saying its wrong**/
 			confirm("wrong username or password");
 			}
-	
+	    }
 		
-}
