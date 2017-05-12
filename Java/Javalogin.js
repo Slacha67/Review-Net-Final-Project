fc1b2cb3aa152d114718c4ceb9685ab4936db9e0
Sacha Ketchum <sacha.mancuso18@alumni.loyola.ca>
2017-05-12 14:04:56 -0400

    Add files via upload

--- Java/Javalogin.js
+++ Java/Javalogin.js
@@ -7,21 +7,22 @@ function login(){
 	
 	/**Here i am naming my variables for the login**/
 	
-	var userarray = ["Anthony", "Anthony2", "Anthony3", "Anthony4", "Sacha", "t"]
-	var passarray = ["Anania", "Smeros", "McCall", "Perna", "Mancuso", "r"]
+	var userarray = ["Anthony", "Anthony2", "Anthony3", "Anthony4", "Sacha", "Antonio"]
+	var passarray = ["Anania", "Smeros", "McCall", "Perna", "Mancuso", "Barrafato"]
 	
-	/**Here the if is checking to see if the username and passowrd is correct**/
-	
+
 	for (j = 0; j <= userarray.length; j++){
+		/**Here the if is checking to see if the username and passowrd is correct**/
 		if ((user === userarray[j]) && (pass === passarray[j])){
             log = true;
+		/** if the login = true then it will stop the code and move towards the other if statement underneath**/
             break;
         }else{
             log = false;
         }
     }
 			
-			
+			/** if the login is true, it will change my HTML code "click the button to login" with "sup" and add the username afterwards**/
 		if (log === true){
 			document.getElementById("userarray").innerHTML = "Sup" + " " + userarray[j];
 		}else{
